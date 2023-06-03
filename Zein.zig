29340741f5639105acf73a6dab7a315b9f27b44c
AndrewCodeDev <andrewmccommons@gmail.com>
2023-06-02 21:57:06 -0700

    removed old file name form Zein.zig

--- Zein.zig
+++ Zein.zig
@@ -2,5 +2,5 @@
 // Zein interface file - import this file directly into your project to begin using Zein.
 
 // import core tensor version... this can be swapped for different tensor implementations.
-const Core = @import("Core/CoreTensor_V1.zig");
 
+// nothing to see here at the moment...
