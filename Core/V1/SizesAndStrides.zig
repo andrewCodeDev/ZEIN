fde1bea34d9ce2e0d9290c85bb59bc08fd9aa338
AndrewCodeDev <andrewmccommons@gmail.com>
2023-06-03 18:10:40 -0700

    Added tensor class and basic tensor semantics.

---
+++ Core/V1/SizesAndStrides.zig
@@ -0,0 +1,214 @@
+
+// Since the sizes and the strides type will be the same int type,
+// we can use the same array, split half-way through for both.
+
+// so... if segment size is 3...
+//            
+//      v - start of first segement
+//      v
+//    [ 0, 1, 2, 3, 4, 5 ]
+//               ^
+//               ^ - Start of the second segment
+
+// This class makes an assumption that each index has an stride and size pair.
+// Therefore, it doesn't make sense to allow only placing a single element.
+
+// V1 makes the assumption that we probably need 5 strides (at most) to be practical
+
+// Another implementation of this similar to Pytorch:C10 is to make a union with a dynamic
+// memory member variable that allows for extending the tensor modes beyond the static 
+// storage size. Unfortunately, that incurs the cost of checking which member is in use.
+
+// A potential work around is to return a slice (or some reference object) and use that.
+// That is cumbersome though, especially for internal implementation details.
+
+////////////////////////////////////////////////////
+// This struct is debatable. It may be better to use
+// a generic pair class instead of this, because I
+// forsee some tedious converions in the future.
+
+pub const SizeAndStride = struct {
+    pub const ValueType = u32;
+    size : ValueType = 0,
+    stride : ValueType = 0
+};
+
+///////////////////////////////////////////////////////
+// Split SizeAndStrides into a contiguous segmented array
+
+fn initSizesAndStrides(comptime n: usize, pairs: ?[n]SizeAndStride) [n * 2]u32 {
+   // this is a hack because init will cause an error
+   // if we don't return the struct type directly    
+    var memory: [n * 2]u32 = undefined;
+
+    if (pairs) |data| 
+    {
+        var i: usize = 0;
+        while(i < n) : (i += 1) {                        
+            memory[i]     = data[i].size;            
+            memory[i + n] = data[i].stride;
+        }
+    }
+    else {
+        @memset(&memory, 0); // zero seems like a sensible default...
+    }
+    return memory;
+}
+
+/////////////////////////////////////////
+// SizesAndStrides Struct Implementation 
+
+ pub fn SizesAndStrides(comptime pair_count: usize) type {
+
+    return struct {
+
+        const Self = @This();
+
+        const SelfPtr = *Self;
+
+        const ConstSelfPtr = *const Self;
+
+        pub const ValueType = SizeAndStride.ValueType;
+    
+        memory: [pair_count * 2]u32 = undefined,
+    
+        // init SizesAndStrides from an optional pair array
+        pub fn init(pairs: ?[pair_count]SizeAndStride) Self {
+            return Self {
+                .memory = initSizesAndStrides(pair_count, pairs),
+            };
+        }
+        
+        //// segmented index getters
+        pub fn getSize(self: Self, i: usize) u32 {
+            return self.memory[i];
+        }
+        pub fn getStride(self: Self, i: usize) u32 {
+            return self.memory[pair_count + i];
+        }
+    
+        //// segemented index setters
+        pub fn setSize(self: SelfPtr, i: usize, value: u32) void {
+            self.*.memory[i] = value;
+        }
+        pub fn setStride(self: SelfPtr, i: usize, value: u32) void {
+            self.*.memory[pair_count + i] = value;
+        }
+    
+        //// pairwise setters/getter
+        pub fn getSizeAndStride(self: Self, i: usize) SizeAndStride {
+            return .{ .size= self.getSize(i), .stride = self.getStride(i) };
+        }
+        pub fn setSizeAndStride(self: SelfPtr, i: usize, pair: SizeAndStride) void {
+            self.*.setSize(i, pair.size);
+            self.*.setStride(i, pair.stride);
+        }
+
+        //// segmented slicing functions
+        pub fn sliceSizes(self: ConstSelfPtr) [] const u32 {
+            return self.*.memory[0..pair_count];
+        }
+        pub fn sliceStrides(self: ConstSelfPtr) [] const u32 {
+            return self.*.memory[pair_count..pair_count * 2];
+        }
+    
+        pub fn copyFrom(self: SelfPtr, other: ConstSelfPtr) void {
+            @memcpy(&self.*.memory, &other.*.memory);        
+        }
+    
+        // type sizes... member functions instead?
+        pub fn segmentSize() usize {
+            return pair_count;
+        }
+        pub fn capacity() usize {
+            return pair_count * 2;
+        }
+    };
+}
+    
+/////////////////////////////////
+//////////// TESTING ////////////
+
+test "Initialization" {
+    const std = @import("std");
+
+    var s1 = SizesAndStrides(5).init([_]SizeAndStride{
+            .{ .size = 100, .stride = 100 },
+            .{ .size = 101, .stride = 101 },
+            .{ .size = 102, .stride = 102 },
+            .{ .size = 103, .stride = 103 },
+            .{ .size = 104, .stride = 104 },
+        });
+
+    var s2 = SizesAndStrides(5).init(null);
+
+    s2.setSizeAndStride(0, .{ .size = 100, .stride = 100 });
+    s2.setSizeAndStride(1, .{ .size = 101, .stride = 101 });
+    s2.setSizeAndStride(2, .{ .size = 102, .stride = 102 });
+    s2.setSizeAndStride(3, .{ .size = 103, .stride = 103 });
+    s2.setSizeAndStride(4, .{ .size = 104, .stride = 104 });
+
+    try std.testing.expect(SizesAndStrides(5).capacity() == 10);
+    try std.testing.expect(SizesAndStrides(5).capacity() == 10);
+    try std.testing.expect(SizesAndStrides(5).segmentSize() == 5);
+    try std.testing.expect(SizesAndStrides(5).segmentSize() == 5);
+
+    try std.testing.expect(s1.getSize(0) == s2.getSize(0));
+    try std.testing.expect(s1.getSize(1) == s2.getSize(1));
+    try std.testing.expect(s1.getSize(2) == s2.getSize(2));
+    try std.testing.expect(s1.getSize(3) == s2.getSize(3));
+    try std.testing.expect(s1.getSize(4) == s2.getSize(4));
+
+    try std.testing.expect(s1.getStride(0) == s2.getStride(0));
+    try std.testing.expect(s1.getStride(1) == s2.getStride(1));
+    try std.testing.expect(s1.getStride(2) == s2.getStride(2));
+    try std.testing.expect(s1.getStride(3) == s2.getStride(3));
+    try std.testing.expect(s1.getStride(4) == s2.getStride(4));
+}
+
+test "Slicing" {
+
+    const std = @import("std");
+
+    var s1 = SizesAndStrides(3).init([_]SizeAndStride{
+            .{ .size = 100, .stride = 200 },
+            .{ .size = 101, .stride = 201 },
+            .{ .size = 102, .stride = 202 },
+        });
+
+    { // test size slice
+        const slice = s1.sliceSizes();
+        try std.testing.expect(slice.len == 3);
+        try std.testing.expect(slice[0] == 100);
+        try std.testing.expect(slice[1] == 101);
+        try std.testing.expect(slice[2] == 102);
+    }
+    { // test stride slice
+        const slice = s1.sliceStrides();
+        try std.testing.expect(slice.len == 3);
+        try std.testing.expect(slice[0] == 200);
+        try std.testing.expect(slice[1] == 201);
+        try std.testing.expect(slice[2] == 202);
+    }    
+}
+
+test "Copying" {
+
+    const std = @import("std");
+
+    var s1 = SizesAndStrides(5).init([_]SizeAndStride{
+            .{ .size = 100, .stride = 100 },
+            .{ .size = 101, .stride = 101 },
+            .{ .size = 102, .stride = 102 },
+            .{ .size = 103, .stride = 103 },
+            .{ .size = 104, .stride = 104 },
+        });
+
+    var s2 = SizesAndStrides(5).init(null);
+
+    s2.copyFrom(&s1);
+
+    for(s1.memory, s2.memory) |i, j| {
+        try std.testing.expect(i == j);
+    }
+}
---
+++ Core/V1/Tensor.zig
@@ -0,0 +1,223 @@
+
+// Here we find the heart of Zein - Tensors. Before proceeding, please read the following:
+
+///////////////////////////////////
+// DESIGN PHILOSOPHY (June 3, 2023)
+
+                // MEMORY, OWNDERSHIP, AND REFERENCING //
+
+// There is no plan to make a distinction between a tensor and a "view" of a tensor.
+// Tensors here are, by design, a way to view data. As such, a different "tensored" view
+// of the same data is just another tensor that shares underlying memory.
+
+// !!! THIS STRONGLY IMPLIES THAT TENSORS DO NOT *OWN* DATA, THEY VIEW IT !!!
+
+// If anything can be said to "own" memory, it is the allocator. Allocators are going 
+// to play an important role in this library (as they do in Zig more generally).
+
+// To create a tensor that has initialized memory is the job of a factory.
+// The design of such a tensor factory, as it were, will be handled in a source
+// file dedicated to that exact job. It is very important that we do not cross
+// responsibilities in this system.
+
+                // ACCESSING TENSOR DATA AND ELEMENTS //
+
+// Tensors, in this system, should not to be mistaken for arrays. Arrays do allow a user
+// to access individual elements and modify them. Tensors (for our purpose) are meant
+// to be used to manipulate data at a batch level. If a user wants to manipulate an
+// individual element, they certainly can by simply using the underlying memory array
+// that a tensor is currently viewing.
+
+// Broadly speaking, the handling of individual elements will be done by algorithms
+// that work at the scope of tensors. Because of this, Zein should supply a varienty
+// of robust batch level operations that allow for these manipulations.
+
+                // TENSORS AS THEY RELATE TO ARRAYS //
+
+// Because of the design descisions outlined above, users should be able to easily
+// make a tensor with their desired dimensions to wrap existing arrays and manipulate
+// them as if they were tensors themselves. This means that a tensor can act like
+// an adapter to already existing memory.
+
+// Because of this, there is not a current plan to enforce that tensors must be of
+// one type or another. It is my hope to provide a generic tensor based interface
+// that can be used on a variety of objects at the user's caution.
+
+// At some point, it may be important to then provide a generic functional interface
+// to provide for further use cases such as generically holding objects that users
+// create themselves. While this is an interesting goal, the scope of V1 is currently
+// focused on integer and floating point numbers. User provided types will have to
+// be reviewed as time goes forward.
+
+pub const SizeAndStride = @import("SizesAndStrides.zig").SizeAndStride;
+
+pub const SizesAndStrides = @import("SizesAndStrides.zig").SizesAndStrides;
+
+// more imports coming soon as they are implemented...
+
+fn sliceProduct(comptime T: type, slice: [] const T) T {
+    var total: T = 1;
+    for(slice) |n| { 
+        total *= n;
+    }
+    return total;    
+}
+
+pub fn Tensor(comptime value_type: type, comptime rank: usize) type {
+
+    return struct {
+
+        const Rank = rank;
+
+        const ValueType = value_type;
+
+        const ValueSlice = []ValueType;
+
+        const Self = @This();
+
+        const SelfPtr = *Self;
+
+        const ConstSelfPtr = * const Self;
+
+        value_slice : ValueSlice,
+        sizes_and_strides : SizesAndStrides(Rank),
+
+        pub fn init(
+            value_slice: ?ValueSlice,
+            sizes_and_strides: ?[Rank]SizeAndStride,
+        ) Self {
+            return Self {
+                .value_slice = if (value_slice) |vs| (vs) else &[_]ValueType{},
+                .sizes_and_strides = SizesAndStrides(Rank).init(sizes_and_strides),
+            };
+        }
+
+        pub fn getSizes(self: ConstSelfPtr) [] const u32 {
+            return self.*.sizes_and_strides.sliceSizes();
+        }
+        pub fn getStrides(self: ConstSelfPtr) [] const u32 {
+            return self.*.sizes_and_strides.sliceStrides();
+        }
+        
+        pub fn valueCapacity(self: ConstSelfPtr) usize {
+            return sliceProduct(SizeAndStride.ValueType, self.*.getSizes());
+        }
+        pub fn valueSize(self: ConstSelfPtr) usize {
+            return self.*.value_slice.len;
+        }
+
+        pub fn atCapacity(self: ConstSelfPtr) bool {
+            return self.*.valueSize() == self.*.valueCapacity();
+        }
+
+        /////////////////////////////////////////
+        // Unchecked Functions Implementations //
+
+        // I understand that these functions are verbose,
+        // but they are meant to communicate to the user
+        // that an operation they are about to do could
+        // invalidate a tensor in some way.
+        
+        // to use this function safely, check that both slice lenghts are
+        // the same and that the capacity is equal to the value length
+        pub fn setValuesUnchecked(self: SelfPtr, values: ValueSlice) void {
+            self.*.value_slice = values;
+        }
+
+        // to use this function safely, check that both tensor value
+        // sizes are the same and that both tensors are at capacity
+        pub fn swapValuesUnchecked(self: SelfPtr, other: SelfPtr) void {
+            var tmp = self.*.value_slice;
+            self.*.value_slice = other.*.value_slice;
+            other.*.value_slice = tmp;
+        }
+
+        // to use this function safely, check that the both tensors are at capacity
+        pub fn swapSizesAndStridesUnchecked(self: SelfPtr, other: SelfPtr) void {
+            // there is probably a faster way to do this
+            var tmp = self.*.sizes_and_strides;
+            self.*.sizes_and_strides = other.*.sizes_and_strides;
+            other.*.sizes_and_strides = tmp;
+        }
+
+        // to use this function safely, check that both tensors are at capacity
+        pub fn swapTensorsUnchecked(self: SelfPtr, other: SelfPtr) void {
+            self.*.swapValuesUnchecked(other);
+            self.*.swapSizesAndStridesUnchecked(other);
+        }
+
+        ///////////////////////////////////////
+        // Checked Functions Implementations //
+
+        // Checked functions only succeed if their guard clauses
+        // are true. Otherwise, they return false and do not
+        // perform the operation. This is to prevent leaving
+        // tensors in an invalid state after the operation.
+
+        pub fn setValuesChecked(self: SelfPtr, values: ValueSlice) bool {
+            // to assure that sizes and strides are not
+            // invalidated, we check size and capacity
+            if(self.*.valueCapacity() != values.len){
+                return false;
+            }
+            self.*.setValuesUnchecked(self, values);
+            return true;
+        }
+
+        pub fn swapValuesChecked(self: SelfPtr, other: SelfPtr) bool {
+            // to assure that sizes and strides are not
+            // invalidated, we check size and capacity
+            if(self.*.valueSize() != other.*.valueSize()){
+                return false;
+            }
+            if(!self.*.atCapacity() or !other.*.atCapacity()) {
+                return false;
+            }
+            self.*.swapValuesUnchecked(other);
+            return true;
+        }
+
+        pub fn swapSizesAndStridesChecked(self: SelfPtr, other: SelfPtr) bool {
+            // we only want to compute these once...
+            const capacity_a = self.valueCapactiy();
+            const capacity_b = other.valueCapactiy();
+
+            // tensors can have different SizesAndStrides
+            // and still share the total value capcity
+            if(capacity_a != capacity_b){
+                return false;
+            }
+            // check that both tensors are at capacity without additional computation
+            if(self.*.ValueSize() != capacity_a  or other.*.ValueSize() != capacity_b) {
+                return false;
+            }
+            self.*.swapSizesAndStridesUnchecked(other);
+            return true;
+        }
+
+        pub fn swapTensorsChecked(self: SelfPtr, other: SelfPtr) bool {
+            // Two tensors do not need to be the same size to be
+            // swapped, we require that they are both at capcity
+            if(!self.*.atCapacity() or !other.*.atCapacity()) {
+                return false;
+            }
+            self.*.swapTensorsUnchecked(other);
+            return true;
+        }
+    };
+}
+
+test "Initialization" {
+
+    const expect = @import("std").testing.expect;
+
+    var x = Tensor(u32, 3).init(null, [_]SizeAndStride{
+        .{ .size = 10, .stride = 10 },
+        .{ .size = 20, .stride = 20 },
+        .{ .size = 30, .stride = 30 }
+    });
+
+    const total: usize = 10 * 20 * 30;
+
+    try expect(total == x.valueCapacity());        
+}
