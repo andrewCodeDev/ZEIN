532d2bf35a94da8a07b62f515ccd28a12cbaeac3
AndrewCodeDev <andrewmccommons@gmail.com>
2023-06-04 00:03:29 -0700

    Removed reference code from Transpose.zig.

--- Core/V1/Transpose.zig
+++ Core/V1/Transpose.zig
@@ -1,41 +1,4 @@
 
-//OPERATOR_SCHEMA(Transpose)
-//    .NumInputs(1)
-//    .NumOutputs(1)
-//    .TensorInferenceFunction([](const OperatorDef& def,
-//                                const vector<TensorShape>& in) {
-//      ArgumentHelper helper(def);
-//      vector<int> axes = helper.GetRepeatedArgument<int>("axes");
-//      vector<TensorShape> out(1);
-//      out[0].set_data_type(in[0].data_type());
-//
-//      if (axes.empty()) {
-//        for (auto axis = in [0].dims().rbegin(); axis != in[0].dims().rend();
-//             ++axis) {
-//          out[0].add_dims(*axis);
-//        }
-//      } else {
-//        auto tensor_size = in[0].dims().size();
-//        auto valid_axes =
-//            std::all_of(axes.begin(), axes.end(), [&tensor_size](int& axis) {
-//              return axis >= 0 && axis < tensor_size;
-//            });
-//
-//        CAFFE_ENFORCE(valid_axes, "Axes argument passed in had invalid values");
-//        CAFFE_ENFORCE(
-//            // NOLINTNEXTLINE(clang-diagnostic-sign-compare)
-//            axes.size() == tensor_size,
-//            "Axes argument passed in had the incorrect size");
-//
-//        // NOLINTNEXTLINE(modernize-loop-convert)
-//        for (auto axis = axes.begin(); axis != axes.end(); ++axis) {
-//          out[0].add_dims(in[0].dims().Get(*axis));
-//        }
-//      }
-//
-//      return out;
-//    })
-
 // make this an enum at some point
 const SizesAndStrides = @import("SizesAndStrides.zig").SizeAndStride;
 
@@ -87,7 +50,7 @@ pub fn transposeToOutput(
         y_d.* = x_d.*;
     }
 
-    // other-permutation branch... modify tensor y
+    // modify SizesAndStrides of tensor y
     if(x_s.ptr != y_s.ptr){
         var i :usize = 0;
         for(permutation) |*p| {
